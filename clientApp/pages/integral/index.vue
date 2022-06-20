d8c5931034aba9907b91dd4a0fb4286366f55643
yingxianqi <1362575166@qq.com>
2022-06-20 08:49:39 +0800

    ..

--- clientApp/pages/integral/index.vue
+++ clientApp/pages/integral/index.vue
@@ -2,82 +2,124 @@
 	<view class="pagebackground">
 		<navigation-bar title="积分中心"></navigation-bar>
 		<view class="uni-padding-wrap uni-common-mt">
-
-			<view class="uni-flex uni-row radios relcount iteralheight">
-				<view class="text" style="width: 200rpx;margin-top: 3%;margin-left: 2%;">
-					<text style="display: flex;">当前积分</text>
-					<text style="color: green;font-size: 20px;">0</text>
+	     <view style="margin-top: 20px;">
+					<el-card class="box-card" >
+					<el-row :gutter="20">
+						<el-col :span="6"> 
+						<div style="font-size: 16px; opacity: 1;font-weight: bold;">
+							当前积分：
+						</div>
+						</el-col>
+						<el-col :span="18">
+							<div style="font-size: 16px; opacity: 1;font-weight: bold;text-align: left;">
+								0
+							</div>
+							<view style="margin-top:10px;text-align: right;">
+								
+							<text class="g-icons"></text></view>
+						</el-col>
+					</el-row> 
+			      </el-card>
 				</view>
-				<view class="text" style="-webkit-flex: 1;flex: 1;margin-top: 10%;margin-left: 50%;">
-					<text class="g-icons"></text>
-				</view>
-			</view>
-
-			<view class="uni-flex uni-row radios relcount iteralorder" style="margin-top: 5%;">
-				<view class="text" style="margin-top: 3%;margin-left: 2%;">
-					<text style="float: left;">
-						<image src="/static/image/iteral/integral-mall.png" style="width: 30px;height: 30px;"></image>
-					</text>
-					<text style="margin-left: 5px;">积分商城</text>
-				</view>
-				<view class="text" style="-webkit-flex: 1;flex: 1;margin-top: 4%;margin-left: 50%;">
-					<text class="g-icons"></text>
-				</view>
-			</view>
-			<view class="uni-flex uni-row radios relcount iteralorder">
-				<view class="text" style="margin-top: 3%;margin-left: 2%;">
-					<text style="float: left;">
-						<image src="/static/image/iteral/integral-order.png" style="width: 30px;height: 30px;"></image>
-					</text>
-					<text style="margin-left: 5px;">积分订单</text>
-				</view>
-				<view class="text" style="-webkit-flex: 1;flex: 1;margin-top: 4%;margin-left: 50%;">
-					<text class="g-icons"></text>
-				</view>
-			</view>
-
-
-			<view class="uni-flex uni-row radios relcount iteralcomplete" style="margin-top: 5%;">
-				<view class="text" style="margin-top: 3%;margin-left: 2%;">
-					<text style="float: left;display: flex;">
-						<image src="/static/image/iteral/integral-credit.png"
-							style="width: 30px;height: 30px;margin-top: 50%;"></image>
-					</text>
-					<text style="display:flex;margin-top:10%;width: 100px;font-weight: 600;margin-left: 20%;">完成查卡</text>
-					<text style="display: flex;width: 190px;font-size: 15px;">完成一次查卡自动获得1积分</text>
-				</view>
-				<view class="text" style="-webkit-flex: 1;flex: 1;margin-top: 7%;margin-left: 13%;">
-					<text><button class="mini-btn " type="primary" size="mini">去完成</button></text>
-				</view>
-			</view>
-			<view class="uni-flex uni-row radios relcount iteralcomplete">
-				<view class="text" style="margin-top: 3%;margin-left: 2%;">
-					<text style="float: left;display: flex;">
-						<image src="/static/image/iteral/integral-repayment.png"
-							style="width: 30px;height: 30px;margin-top: 50%;"></image>
-					</text>
-					<text style="display:flex;margin-top:10%;width: 100px;font-weight: 600;margin-left: 20%;">完成代还</text>
-					<text style="display: flex;width: 190px;font-size: 15px;">完成一次代还贷款自动获得20积分</text>
-				</view>
-				<view class="text" style="-webkit-flex: 1;flex: 1;margin-top: 7%;margin-left: 13%;">
-					<text><button class="mini-btn " type="primary" size="mini">去完成</button></text>
-				</view>
-			</view>
-			<view class="uni-flex uni-row radios relcount iteralcomplete">
-				<view class="text" style="margin-top: 3%;margin-left: 2%;">
-					<text style="float: left;display: flex;">
-						<image src="/static/image/iteral/integral-share.png"
-							style="width: 30px;height: 30px;margin-top: 50%;"></image>
-					</text>
-					<text style="display:flex;margin-top:10%;width: 100px;font-weight: 600;margin-left: 20%;">邀请好友</text>
-					<text style="display: flex;width: 190px;font-size: 15px;">成功邀请一名好友成为平台正式会员自动获得100积分</text>
-				</view>
-				<view class="text" style="-webkit-flex: 1;flex: 1;margin-top: 7%;margin-left: 13%;">
-					<text><button class="mini-btn " type="primary" size="mini">去完成</button></text>
-				</view>
-			</view>
 			
-
+			
+			<view style="margin-top: 20px;">
+					<el-card class="box-card" >
+					<el-row :gutter="20">
+						<el-col :span="3"> 
+						<image src="/static/image/iteral/integral-mall.png" style="width: 30px;height: 30px;"></image>
+						</el-col>
+						<el-col :span="21">
+							<div style="font-size: 16px; opacity: 1;font-weight: bold;">
+								积分商城
+							</div>
+							<view style="margin-top:10px;text-align: right;">
+								
+							<text class="g-icons"></text></view>
+						</el-col>
+					</el-row> 
+			      </el-card>
+				</view>
+			<view style="margin-top: 0px;">
+					<el-card class="box-card" >
+					<el-row :gutter="20">
+						<el-col :span="3"> 
+						<image src="/static/image/iteral/integral-order.png" style="width: 30px;height: 30px;"></image>
+						</el-col>
+						<el-col :span="21">
+							<div style="font-size: 16px; opacity: 1;font-weight: bold;">
+								积分订单
+							</div>
+							<view style="margin-top:10px;text-align: right;">
+								
+							<text class="g-icons"></text></view>
+						</el-col>
+					</el-row> 
+			      </el-card>
+				</view>
+			
+			
+<view style="margin-top: 20px;">积分任务</view>	
+<view style="margin-top: 9px;">
+		<el-card class="box-card" >
+		<el-row :gutter="20">
+			<el-col :span="3"> 
+				<image src="/static/image/iteral/integral-credit.png"
+					style="width: 30px;height: 30px;"></image>
+			</el-col>
+			<el-col :span="21">
+				<div style="font-size: 16px; opacity: 1;font-weight: bold;">
+					完成查卡
+				</div>
+				<view style="font-size: 8px"><span>完成一次查卡自动获得1积分</span></view>
+				
+				<view style="margin-top:10px;text-align: right;">
+					
+				<span> <el-button type="primary">去完成</el-button> </span></view>
+			</el-col>
+		</el-row> 
+      </el-card>
+	</view>
+<view style="margin-top: 0px;">
+		<el-card class="box-card" >
+		<el-row :gutter="20">
+			<el-col :span="3"> 
+				<image src="/static/image/iteral/integral-repayment.png"
+					style="width: 30px;height: 30px;"></image>
+			</el-col>
+			<el-col :span="21">
+				<div style="font-size: 16px; opacity: 1;font-weight: bold;">
+					完成代还
+				</div>
+				<view style="font-size: 8px"><span>完成一次代还贷款自动获得20积分</span></view>
+				
+				<view style="margin-top:10px;text-align: right;">
+					
+				<span> <el-button type="primary">去完成</el-button> </span></view>
+			</el-col>
+		</el-row> 
+      </el-card>
+	</view>
+	<view style="margin-top: 0px;">
+			<el-card class="box-card" >
+			<el-row :gutter="20">
+				<el-col :span="3"> 
+				<image src="/static/image/iteral/integral-share.png"
+					style="width: 30px;height: 30px;"></image>
+				</el-col>
+				<el-col :span="21">
+					<div style="font-size: 16px; opacity: 1;font-weight: bold;">
+						邀请好友
+					</div>
+					<view style="font-size: 8px"><span>成功邀请一名好友成为平台正式会员自动获得100积分</span></view>
+					
+					<view style="margin-top:10px;text-align: right;">
+						
+					<span> <el-button type="primary">去完成</el-button> </span></view>
+				</el-col>
+			</el-row> 
+	      </el-card>
+		</view>
 
 
 		</view>
@@ -99,27 +141,23 @@
 		text-align: center
 	}
 
-	.iteralheight {
-		height: 6em;
+</style>
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+} 
+.logo {
+		width: 100%;
+		height: 100px;
 	}
-
-	.iteralorder {
-		height: 3.5em;
-	}
-
-	.iteralcomplete {
-		height: 5.5em;
-	}
-
-	.relcount {
-		/* border: 1px solid darkgray; */
-		border-width: 0 0 0 0;
-		border-style: solid;
-		border-color: darkgray;
-		background-color: white;
-	}
-
-	.radios {
-		border-radius: 9px;
-	}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
 </style>
