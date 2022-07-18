24a4617f232d1914fd940456f54cbef1fe93d820
loct@vip.qq.com <H51534227d>
2022-07-18 14:09:31 +0800

    后台 修改登录页面样式

--- adminApp/src/router/routers.js
+++ adminApp/src/router/routers.js
@@ -1,49 +1,49 @@
-import {createRouter,createWebHashHistory} from 'vue-router' 
+import {createRouter, createWebHashHistory} from 'vue-router'
 //-------路由加载都采用赖加载,避免首页加载页面过多出现卡顿,测试
 //  const App =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/demo/demorouter/demo.vue')
- const App2 =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/demo/demorouter/demoto.vue')
- const App3 =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/demo/demorouter/demo.vue')
+const App2 = () => import (/* webpackChunkName: "pageFirst" */ './../vues/demo/demorouter/demoto.vue')
+const App3 = () => import (/* webpackChunkName: "pageFirst" */ './../vues/demo/demorouter/demo.vue')
 //-------路由加载都采用赖加载,避免首页加载页面过多出现卡顿，测试
-const IndexPath =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/creditindex/index.vue')
-const LoginPath =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/user/login.vue')
-const RegisterPath =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/user/register.vue') 
-const UserAgreementPath =()=>import (/* webpackChunkName: "pageFirst" */ './../vues/user/useragreement.vue') 
+const IndexPath = () => import (/* webpackChunkName: "pageFirst" */ './../vues/creditindex/index.vue')
+const LoginPath = () => import (/* webpackChunkName: "pageFirst" */ './../vues/user/login.vue')
+const RegisterPath = () => import (/* webpackChunkName: "pageFirst" */ './../vues/user/register.vue')
+const UserAgreementPath = () => import (/* webpackChunkName: "pageFirst" */ './../vues/user/useragreement.vue')
 //创建路由, hash 模式。
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes:[
-    { path: '', redirect:"/indexPath" },
-    {
-       path: '/indexPath', component: IndexPath,
-       children:[
-        {path:'',redirect:"/indexPath/axioxdemo"},
-        {path:'axioxdemo',component:App3,name:"toapp3"},
-        {path:'vuedemoto',component:App2,name:"toapp2"}
-      
-       ]
-    },
-   { path: '/login', component: LoginPath,name:"LoginPath"  },
-   { path: '/register', component: RegisterPath,name:"RegisterPath"  },
-   { path: '/register', component: UserAgreementPath,name:"UserAgreementPath"  },
+    history: createWebHashHistory(),
+    routes: [
+        {path: '', redirect: "/indexPath"},
+        {
+            path: '/indexPath', component: IndexPath,
+            children: [
+                {path: '', redirect: "/indexPath/axioxdemo"},
+                {path: 'axioxdemo', component: App3, name: "toapp3"},
+                {path: 'vuedemoto', component: App2, name: "toapp2"}
 
-  ]
+            ]
+        },
+        {path: '/login', component: LoginPath, name: "LoginPath"},
+        {path: '/register', component: RegisterPath, name: "RegisterPath"},
+        {path: '/register', component: UserAgreementPath, name: "UserAgreementPath"},
+
+    ]
 })
-export default{
-  router,
-  install(Vue){
-  //添加路由防守,防止未登录就随意进入系统页面,登录注册页面除外
-router.beforeEach((to,form,next)=>{
-  if(to.path.indexOf('/login') !=-1||to.path.indexOf('/register') !=-1){
-    return next();
-  }
-  //Vue.ls.set('Access-Token','')
-  var token=Vue.ls.get('Access-Token');
-  if(!token){
-    return next('/login');
-  }else{
-    return next();
-  }
-    
-  })
-  }
+export default {
+    router,
+    install(Vue) {
+        //添加路由防守,防止未登录就随意进入系统页面,登录注册页面除外
+        router.beforeEach((to, form, next) => {
+            if (to.path.indexOf('/login') != -1 || to.path.indexOf('/register') != -1) {
+                return next();
+            }
+            //Vue.ls.set('Access-Token','')
+            var token = Vue.ls.get('Access-Token');
+            if (!token) {
+                return next('/login');
+            } else {
+                return next();
+            }
+
+        })
+    }
 }
--- adminApp/src/vues/user/login.vue
+++ adminApp/src/vues/user/login.vue
@@ -1,98 +1,111 @@
 <template>
-  <div class="main">
-    <div class="title">用户登录系统</div>
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px">
-        <el-form-item  label="用户：" prop="Username" class="fontsize" >
-            <el-input v-model="ruleForm.Username" autocomplete="off" placeholder="请输入用户名" ></el-input>
-        </el-form-item>
-        <el-form-item label="密码：" prop="Password" >
-            <el-input type="password" v-model="ruleForm.Password" autocomplete="off"  placeholder="请输入密码" ></el-input>
-        </el-form-item>
-        <el-form-item >
-            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-            <el-button type="primary"  @click="register()">注册</el-button>
-        </el-form-item>
-    </el-form>
-    </div>
+	<div class="flex-row-center w100 h100" style="background-color: #409eff">
+		<div class="login-back flex-column-center">
+		<div class="title">用户登录系统</div>
+		<el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px">
+			<el-form-item label="用户：" prop="Username" class="fontsize">
+				<el-input v-model="ruleForm.Username" autocomplete="off" placeholder="请输入用户名"></el-input>
+			</el-form-item>
+			<el-form-item label="密码：" prop="Password">
+				<el-input type="password" v-model="ruleForm.Password" autocomplete="off" placeholder="请输入密码"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+				<el-button type="primary" @click="register()">注册</el-button>
+			</el-form-item>
+		</el-form>
+		</div>
+	</div>
 </template>
- 
+
 <script>
-    export default {
-      name: "LoGin",
-        data() {
-            
-            var validateUsername= (rule, value, callback) => {
-                if (value === '') {
-                    callback(new Error('请输入用户名!'));
-                } else {
-                    callback();
-                }
-            };
-           
-             var validatePassword = (rule, value, callback) => {
-                if (value === '') {
-                    callback(new Error('请输入密码!'));
-                } else {
-                    callback();
-                }
-            }
-            return {
-                ruleForm: {
-                    Username: '',
-                    Password: ''
-                },
-                rules: {
-                    Username: [
-                        { validator: validateUsername, trigger: 'blur' }
-                    ],
-                    Password: [
-                        { validator: validatePassword, trigger: 'blur' }
-                    ]
-                }
-            };
-        },
-        methods: {
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                if (valid) {
-                //开始提交登录
-                this.$Http.post('/Account/UserLogin',this.ruleForm)
-                    .then(res=>{
-                        var result=res.data
-                        var token=result.data.token
-                        this.appls.set('Access-Token',token)
-                        this.$router.push({ path: '/indexPath'})
-                        }) .catch((err) => {
-                       console.log(err)
-                    });
+	export default {
+		name: "LoGin",
+		data() {
+
+			var validateUsername = (rule, value, callback) => {
+				if (value === '') {
+					callback(new Error('请输入用户名!'));
+				} else {
+					callback();
+				}
+			};
+
+			var validatePassword = (rule, value, callback) => {
+				if (value === '') {
+					callback(new Error('请输入密码!'));
+				} else {
+					callback();
+				}
+			}
+			return {
+				ruleForm: {
+					Username: '',
+					Password: ''
+				},
+				rules: {
+					Username: [
+						{validator: validateUsername, trigger: 'blur'}
+					],
+					Password: [
+						{validator: validatePassword, trigger: 'blur'}
+					]
+				}
+			};
+		},
+		methods: {
+			submitForm(formName) {
+				this.$refs[formName].validate((valid) => {
+					if (valid) {
+						//开始提交登录
+						this.$Http.post('/Account/UserLogin', this.ruleForm)
+							.then(res => {
+								var result = res.data
+								var token = result.data.token
+								this.appls.set('Access-Token', token)
+								this.$router.push({path: '/indexPath'})
+							}).catch((err) => {
+							console.log(err)
+						});
 
 
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
-                });
-            },
-            register() {
-                this.$router.push({ name: 'RegisterPath'})
-            }
-        }
-    }
+					} else {
+						console.log('error submit!!');
+						return false;
+					}
+				});
+			},
+			register() {
+				this.$router.push({name: 'RegisterPath'})
+			}
+		}
+	}
 </script>
- <style>
-  .main{
-  width: 290px!important;
-  margin-left: 40%;
-  margin-top: 10%;
+<style>
+	@import "/src/commcss/flex.css";
+	@import "/src/commcss/background.css";
 
-}
-.title{
-  font-size: 26px;
-  font-weight: bold;
-  color: #353b42;
-  margin: 25px 0;
-  font-family: PingFangSC-Medium, PingFang SC;
-}
+	.main {
+		width: 290px !important;
+		margin-left: 40%;
+		margin-top: 10%;
+
+	}
+
+	.login-back {
+		padding: 50px;
+		box-shadow: 0 0 10px rgba(0,0,0,0.1);
+		background-color: white;
+		border-radius: 10px;
+	}
+
+	.title {
+		font-size: 26px;
+		font-weight: bold;
+		color: #353b42;
+		margin: 25px 0;
+		font-family: PingFangSC-Medium, PingFang SC;
+	}
 
 
- </style>
+</style>
