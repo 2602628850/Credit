c32564e7a804c158abc2cb1090ae6c5818f1cf4a
yingxianqi <1362575166@qq.com>
2022-07-29 22:50:46 +0800

    。。

--- clientApp/common/js/API/BaseUrl.js
+++ clientApp/common/js/API/BaseUrl.js
@@ -1,13 +1,13 @@
 // #ifdef H5
 console.log('h5')
 // 请求域名http://localhost:8003/v1
-let host = 'http://api.credit.ceshi-api.com/v1';
-//let host = 'http://localhost:8003/v1';
+//let host = 'http://credit-api.ceshi-api.com/v1';
+let host = 'http://localhost:8003/v1';
 // #endif
 // #ifdef APP-PLUS
 console.log('app')
 // 请求域名
- let host = 'http://api.credit.ceshi-api.com/v1';
+ let host = 'http://credit-api.ceshi-api.com/v1';
 //let host = '移动端编译这个';
 // #endif
 
